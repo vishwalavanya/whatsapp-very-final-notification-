971a9a296709abd58da7eafe643fdd991a438626
vishwa <123007235+vishwalavanya@users.noreply.github.com>
2025-10-01 16:25:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/send-whatsapp', async (req, res) => {
     const msg = await client.messages.create({
       from: `whatsapp:${twilioPhone}`,
       to: 'whatsapp:+919944490717',
-      body: `New message from Ammu: ${message}`
+      body: message
     });
 
     res.json({ success: true, sid: msg.sid });
